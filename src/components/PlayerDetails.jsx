58e1c9ef8385940ba114b25d280825873420af72
Chase Terry <chase.terry87@gmail.com>
2023-11-16 15:24:24 -0600

    Fixed player details calculations

--- src/components/PlayerDetails.jsx
+++ src/components/PlayerDetails.jsx
@@ -1,10 +1,15 @@
 function PlayerDetails({ playerWins, playerLosses }) {
-  playerWins = parseInt(playerWins)
-  playerLosses = parseInt(playerLosses)
+  let displayPlayerWins = playerWins ?? 0
+  let displayPlayerLosses = playerLosses ?? 0
+  displayPlayerWins = parseInt(displayPlayerWins)
+  displayPlayerLosses = parseInt(displayPlayerLosses)
 
   let playerWinRatio = 0
-  if (playerWins > 0 || playerLosses > 0) {
-    playerWinRatio = (playerWins / (playerWins + playerLosses)).toFixed(2)
+  if (displayPlayerWins > 0 || displayPlayerWins > 0) {
+    playerWinRatio = (
+      displayPlayerWins /
+      (displayPlayerWins + displayPlayerLosses)
+    ).toFixed(2)
   }
 
   return (
@@ -12,8 +17,8 @@ function PlayerDetails({ playerWins, playerLosses }) {
       <div className='flex flex-col gap-1 text-white font-semibold bg-green-800 p-4 rounded-lg text-center border border-green-950'>
         <h3 className='font-bold text-lg uppercase'>Player Details</h3>
         <div className='flex items-center justify-between gap-6 text-xs'>
-          <span>Wins: {playerWins}</span>
-          <span>Losses: {playerLosses}</span>
+          <span>Wins: {playerWins ?? 0}</span>
+          <span>Losses: {playerLosses ?? 0}</span>
           <span>Ratio: {playerWinRatio}</span>
         </div>
       </div>
