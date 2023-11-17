900640f337657cb69631ce4c7a0e9dc8c2552ac1
Chase Terry <chase.terry87@gmail.com>
2023-11-16 19:24:40 -0600

    VIsual changes

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -2,7 +2,7 @@ function Controls({ hit, stay, exit }) {
   return (
     <>
       <div className='w-full'>
-        <div className='text-sm md:text-xl flex flex-col sm:flex-row items-center justify-center gap-2 md:gap-4 p-2 md:p-3 bg-green-800 rounded-lg border border-green-950'>
+        <div className='text-sm md:text-xl flex flex-row items-center justify-center gap-2 md:gap-4 p-2 md:p-3 bg-green-800 rounded-lg border border-green-950'>
           <button
             className='w-full border border-green-950 uppercase font-bold bg-white text-green-800 px-4 py-2 rounded-md hover:bg-gray-200 hover:text-green-900 transition duration-200 ease-in-out'
             onClick={hit}
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -20,7 +20,7 @@ function Table({
 }) {
   return (
     <>
-      <div className='w-full flex flex-col gap-6 items-center justify-center'>
+      <div className='min-h-screen w-full flex flex-col gap-6 items-center justify-between'>
         <PlayerDetails playerWins={playerWins} playerLosses={playerLosses} />
         <Dealer
           hand={dealerHand}
